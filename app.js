fa3fc85e52428df445e35945601bd4cbaf9b02ae
Ehmaed Alhams <ehmaedalzhar@gmail.com>
2022-08-31 22:11:11 +0300

    initial commit.

---
+++ app.js
@@ -0,0 +1,41 @@
+// let arr = [1, 2, 3]
+
+// function app(msg) {
+//     return msg;
+// };
+
+// // export {
+// //     arr,
+// //     app
+// // }
+
+
+// export {
+//     arr as Array,
+//     app as Application
+// }
+
+// let a = 100;
+// let b = 200;
+
+// export {
+//     a, b
+// }
+
+// export default function fun(msg) {
+//     return msg;
+// };
+
+
+// class User{
+// 	constructor(name, age) {
+// 		this.name = name;
+// 		this.age = age;
+// 	}
+
+// 	info() {
+// 		console.log(`Your Name: ${this.name} and your age: ${this.age * 365}`);
+// 	};
+// };
+
+// export {User}
---
+++ test.js
@@ -0,0 +1,14 @@
+// import fun, {a, generateFunNumber } from "./main.js";
+
+// // for (let value of generateFunNumber()) {
+// //     console.log(value)
+// // };
+
+// // console.log(fun());
+
+// console.log(a)
+
+// import * as all from "../ThirdProject/Project-Template-3/js/main.js";
+
+// all.generateFunNumber().next();
+// console.log(all.generateFunNumber)
