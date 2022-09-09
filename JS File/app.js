c46e37947504bb6fea8c80986e5ae3d4805122ed
Ehmaed Alhams <ehmaedalzhar@gmail.com>
2022-09-09 18:19:16 +0300

    update

--- JS File/app.js
+++ JS File/app.js
@@ -39,3 +39,47 @@
 // };
 
 // export {User}
+
+// ANCHOR: JSON course at 9/8/2022
+// NOTE: this course derived
+
+// import data from "../JSON & AJAX File/data.jsonc" assert {type: 'json'};
+
+
+// console.log(data.Array[1]);
+    // // JSON Obj
+    // const obj = '{"user": "Ehmaed", "age": "26"}'
+    // let objParse = JSON.parse(obj);
+    
+    // // string from server
+    // console.log(obj);
+    // console.log(typeof (obj));
+
+    // // object to modifier.
+    // console.log(objParse);
+    // console.log(typeof (objParse));
+
+    // let jsonObj = JSON.stringify(objParse);
+    
+    // console.log(jsonObj);
+    // console.log(typeof (jsonObj));
+
+
+// $(function () {
+//     $.getJSON("/JSON & AJAX File/data.jsonc", data,
+//         function (data) {
+//             console.log(data);
+//             // console.log(textStatus);
+//             // console.log(jqXHR);
+//         }
+//     );
+// });
+
+$(function () {
+    $('#show').click(function () { 
+        // e.preventDefault();
+        // $('#show').hide()
+        console.log('hide h1');
+        
+    });
+});
